b8086931d1e616749427027800e9374adbcf11dd
Bikash Subedi <biikashsubedi@gmail.com>
2019-03-25 00:00:03 +0545

    My First App

--- Week 1/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ Week 1/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -8,19 +8,22 @@ import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+        // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+
     }
 
 }
