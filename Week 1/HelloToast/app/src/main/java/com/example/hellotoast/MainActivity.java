8b0eb4edd158e430c4ef5a9626b27371baf48852
Bikash Subedi <biikashsubedi@gmail.com>
2019-03-25 15:02:32 +0545

    Hello Toast

--- Week 1/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ Week 1/HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hellotoast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
-        mCount++;
+        ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
