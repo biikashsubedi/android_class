50fd6bb257305bba2138f81e5c1fe71d66624056
Bikash Subedi <biikashsubedi@gmail.com>
2019-03-29 23:43:25 +0545

    Two Activities Lifecycle

---
+++ Week 2/TwoActivities (Lifecycle)/app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class SecondActivity extends AppCompatActivity {
+    // Unique tag for the intent reply.
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+
+    // EditText for the reply.
+    private EditText mReply;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // Initialize view variables.
+        mReply = findViewById(R.id.editText_second);
+
+        // Get the intent that launched this activity, and the message in
+        // the intent extra.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Put that message into the text_message TextView
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+
+    public void returnReply(View view) {
+        // Get the reply message from the edit text.
+        String reply = mReply.getText().toString();
+
+        // Create a new intent for the reply, add the reply message to it
+        // as an extra, set the intent result, and close the activity.
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
+        finish();
+    }
+}
